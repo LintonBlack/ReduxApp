90eff09198a924c73f8667193f716c0c897d5cee
Phil <phil@metrodesk.com>
2016-11-11 12:25:41 +0000

    set up Route and Router

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -3,7 +3,9 @@ import React from "react";
 class Greetings extends React.Component {
 	render() {
 		return (
+			<div className="jumbotron">
 				<h1>Michel from react</h1>
+			</div>
 			)
 		}
 	}
---
+++ client/components/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router";
+
+
+class NavigationBar extends React.Component {
+	render() {
+		return (
+				<nav className="navbar navbar-default">
+  					<div className="container-fluid">
+    					<div className="navbar-header">
+    						<Link to="/" className="navbar-brand">Home</Link>     						
+    					</div>
+    					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      						<ul className="nav navbar-nav navbar-right">
+        						<Link to="/signup" className="navbar-brand">SIgn up</Link>  
+       	   					</ul>      
+				    	</div>
+				  	</div>
+				</nav>
+			)
+		}
+	}
+
+
+export default NavigationBar;
