e315eab71a645b68c2614a9ce4c35493be0a97fd
Phil <phil@metrodesk.com>
2016-11-15 12:08:54 +0000

    Validate username and email uniqueness on server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,37 +1,80 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcryptjs';
+/*Bad approach returning 2 promises import Promise from 'bluebird';*/
+import isEmpty from 'lodash/isEmpty';
 
 import User from '../models/user';
 
 
 let router = express.Router();
 
+function validateInput(data, otherValidations) {
+	let { errors } = otherValidations(data);
+ /*Right approach*/
+ return User.query({
+ 	where: { email: data.email},
+ 	orWhere: { username: data.username}
+ }).fetch().then(user => {
+ 	if (user) {
+ 		if (user.get('username') === data.username) { 
+ 			errors.username = 'There is user with such username';
+ 		}
+
+ 		if (user.get('email') === data.email) { 
+ 			errors.email = 'There is user with such email';
+ 		}
+ 	}
+
+ 	//then return errors
+ 	return {
+				errors,
+				isValide: isEmpty(errors)
+			}
+ })
+
+ /* Bad approach returning 2 promises
+	return Promise.all([
+		User.where({ email:data.email}).fetch().then(user => {
+				if (user) { errors.email = 'There is user with such email'}
+			}),
+		User.where({ username:data.username}).fetch().then(user => {
+				if (user) { errors.username = 'There is user with such username'}
+			})
+		]).then(() => {
+			return {
+				errors,
+				isValide: isEmpty(errors)
+			}
+		})
+		*/
+	
+
+
+
+	
+}
+//commonValidations
+
 router.post('/', (req,res) => {
-	//simulate setTimeout
-	/*setTimeout(() => {
-		const { errors, isValid } = validateInput(req.body);
 
-		if (!isValid) {
-			res.status(400).json(errors);
-		}
-	}, 5000)*/
+	validateInput(req.body, commonValidations).then(({ errors, isValid}) => {
+		if (isValid) {
+		      const { username, password, timezone, email } = req.body;
+		      const password_digest = bcrypt.hashSync(password, 10);
 
-	const { errors, isValid } = validateInput(req.body);
+		      User.forge({
+		        username, timezone, email, password_digest
+		      }, { hasTimestamps: true }).save()
+		        .then(user => res.json({ success: true }))
+		        .catch(err => res.status(500).json({ error: err }));
 
-	 if (isValid) {
-      const { username, password, timezone, email } = req.body;
-      const password_digest = bcrypt.hashSync(password, 10);
+		    } else {
+				res.status(400).json(errors);
+			}
+	});
 
-      User.forge({
-        username, timezone, email, password_digest
-      }, { hasTimestamps: true }).save()
-        .then(user => res.json({ success: true }))
-        .catch(err => res.status(500).json({ error: err }));
 
-    } else {
-		res.status(400).json(errors);
-	}
 	
 })
 
